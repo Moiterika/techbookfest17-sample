b5abc43ed81e0bd33325a9296b457e6b6693b628
saeki-hisafumi <saeki-hisafumi@moiterika.jp>
2024-10-12 12:17:23 +0000

    FIX HTMX sample

--- my-todo/src/pages/api/sample.ts
+++ my-todo/src/pages/api/sample.ts
@@ -3,5 +3,14 @@ import type { APIRoute } from "astro";
 export const POST: APIRoute = async ({ request }) => {
     const formData = await request.formData();
     const myName = formData.get("my-name");
+    if (myName == "hoge") {
+        return new Response(
+            `<div id="error-message" hx-swap-oob="true">hogeはダメです。</div>`,
+            { status: 200 }
+        );
+    }
+    if (myName == "fuga") {
+        return new Response(`<div>fugaはダメです。</div>`, { status: 400 });
+    }
     return new Response(`<div>${myName}</div>`, { status: 200 });
 };
