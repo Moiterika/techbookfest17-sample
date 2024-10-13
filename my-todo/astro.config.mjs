8c2a026b3f2e3ce21436257775e51bc3694774d1
saeki-hisafumi <saeki-hisafumi@moiterika.jp>
2024-10-13 01:13:32 +0000

    ADD @astrojs/alpinejs

--- my-todo/astro.config.mjs
+++ my-todo/astro.config.mjs
@@ -3,11 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import node from '@astrojs/node';
 
+import alpinejs from '@astrojs/alpinejs';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
 
   adapter: node({
     mode: 'standalone'
-  })
+  }),
+
+  integrations: [alpinejs()]
 });
