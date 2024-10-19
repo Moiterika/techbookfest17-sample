8afbc9cf954be5ecb5597684087279c8135bb2bc
saeki-hisafumi <saeki-hisafumi@moiterika.jp>
2024-10-19 16:22:05 +0900

    MOD Moved CSS definitions to src/styles/form.css.ts for better organization

---
+++ my-todo/src/styles/form.css.ts
@@ -0,0 +1,35 @@
+import { css } from "../../styled-system/css";
+
+export const inputStyle = css({
+    border: "1px solid #818cf8",
+    padding: "8px",
+    borderRadius: "4px",
+    _focus: {
+        borderColor: "#4338ca",
+        outline: "none",
+    },
+});
+
+export const buttonStyle = css({
+    padding: "8px 16px",
+    fontSize: "16px",
+    fontWeight: "bold",
+    color: "white",
+    backgroundColor: "indigo.500",
+    border: "none",
+    borderRadius: "4px",
+    cursor: "pointer",
+    transition: "background-color 0.3s ease",
+
+    "&:hover": {
+        backgroundColor: "indigo.700",
+    },
+    "&:active": {
+        backgroundColor: "indigo.500",
+    },
+    "&:disabled": {
+        backgroundColor: "indigo.100",
+        cursor: "not-allowed",
+        pointerEvents: "none",
+    },
+});
